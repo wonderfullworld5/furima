f103f16a4b1bc907130e90d196868bf3a29bae12
wonderfullworld5 <bbb893576@gmail.com>
2024-02-29 09:05:27 +0900

    Basic認証

--- app/controllers/furima_controller.rb
+++ app/controllers/furima_controller.rb
@@ -1,5 +1,10 @@
 class FurimaController < ApplicationController
   def index
+<<<<<<< HEAD
     @tweets = furima.all
+=======
+    @furimas = Furima.all
+    @furimas ||= [] # @furimasがnilの場合、空の配列を代入する
+>>>>>>> parent of 3b9fb57 (ユーザー認証)
   end
 end
