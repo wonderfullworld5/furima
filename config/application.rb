976a452e50e3f9d60a01b6ebdd49e6165d6c33ee
AH <AH@MacBook-Pro.local>
2024-02-29 12:34:40 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,15 @@
-Rails.application.routes.draw do
-  devise_for :users
-  # Other routes definitions
-end
-
-require "rails/all"
-require 'devise'
-
-Bundler.require(*Rails.groups)
-
 module Furima40110
   class Application < Rails::Application
-    config.load_defaults 7.0
+  # Initialize configuration defaults for originally generated Rails version.
+  #config.load_defaults 7.0
+  
+  
+  # Configuration for the application, engines, and railties goes here.
+  #
+  # These settings can be overridden in specific environments using the files
+  # in config/environments, which are processed later.
+  #
+  # config.time_zone = "Central Time (US & Canada)"
+  # config.eager_load_paths << Rails.root.join("extras")
   end
-end
+  end
