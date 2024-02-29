f804ad2757194051cf2e231a449dc3952e29e922
AH <AH@MacBook-Pro.local>
2024-02-29 12:20:14 +0900

    修正

--- config/application.rb
+++ config/application.rb
@@ -3,28 +3,13 @@ Rails.application.routes.draw do
   # Other routes definitions
 end
 
-# require_relative "boot"  # 不要な行を削除しました
-
 require "rails/all"
 require 'devise'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module Furima40110
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,28 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :furimas, only: :index
-  root 'welcome#index'
+  # Other routes definitions
 end
 
+# require_relative "boot"  # 不要な行を削除しました
+
+require "rails/all"
+require 'devise'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Furima40110
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 7.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+  end
+end
