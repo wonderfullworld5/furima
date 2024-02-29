6993c02330710fff18d474d928992d12e2aafc95
wonderfullworld5 <bbb893576@gmail.com>
2024-02-29 09:22:28 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-=======
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'furima#index'
->>>>>>> parent of 3b9fb57 (ユーザー認証)
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :furimas, only: :index
+  root 'welcome#index'
 end
+
