3e6e447ea33cf5f3c744de597a8e542b13730bcb
wonderfullworld5 <bbb893576@gmail.com>
2024-03-10 12:51:47 +0900

    実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_09_201729) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_10_012520) do
   create_table "furimas", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_09_201729) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
   end
 
 end
