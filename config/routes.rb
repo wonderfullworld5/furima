9a95d443aa3c6052f16166cfe3982dcfeb489b34
wonderfullworld5 <bbb893576@gmail.com>
2024-03-14 05:38:49 +0900

    新規登録

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, path: 'users', controllers: {
     sessions: 'users/sessions'
   }, path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
-  
+
+
+
   root 'items#index'
 end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_10_012520) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_12_222526) do
   create_table "furimas", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,7 +22,10 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_10_012520) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "nickname"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
