57bc0e48f04792629a9a27a09af63d3ba527e3de
wonderfullworld5 <bbb893576@gmail.com>
2024-03-16 07:51:34 +0900

    link_toに変更

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,4 @@
 # config/initializers/session_store.rb
 
-# セッションのストアをActiveRecordに変更する場合
-Rails.application.config.session_store :active_record_store, key: '_your_app_session'
-
-# セッションの有効期限を1週間に設定する場合
+# セッションのストアをActiveRecordに変更し、有効期限を1週間に設定する場合
 Rails.application.config.session_store :active_record_store, key: '_your_app_session', expire_after: 1.week
