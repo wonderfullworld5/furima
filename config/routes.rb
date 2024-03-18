15bcaf8f7402e9732238444020ada4a74719d9c9
wonderfullworld5 <bbb893576@gmail.com>
2024-03-18 14:52:20 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', controllers: {
-    sessions: 'users/sessions'
-  }, path_names: { sign_in: 'sign_in', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'signup' }
-
-  # ユーザーの詳細ページへのルートを追加
-  resources :users, only: [:show]
- 
-  # CSSファイルへのルートを追加
- get 'styles.css', to: redirect('assets/styles.css')
+  devise_for :users
   root 'items#index'
 end
 
