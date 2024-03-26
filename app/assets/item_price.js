3239b9ad1be53fe4a993f4b8676d9b307d2df769
wonderfullworld5 <bbb893576@gmail.com>
2024-03-26 10:30:03 +0900

    JS設定

--- app/assets/item_price.js
+++ app/assets/item_price.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const priceInput = document.getElementById("item-price");
-    const commissionOutput = document.getElementById("add-tax-price");
-    const profitOutput = document.getElementById("profit");
-    priceInput.addEventListener("input", function() {
-
-    });
-    // ここから処理を書いていきます
-  });
+  const priceInput = document.getElementById("item-price");
+  const commissionOutput = document.getElementById("add-tax-price");
+  const profitOutput = document.getElementById("profit");
   
+  priceInput.addEventListener("input", function() {
+    const price = priceInput.value;
+    const commission = Math.floor(price * 0.1);
+    const profit = price - commission;
+    
+    commissionOutput.textContent = commission;
+    profitOutput.textContent = profit;
+  });
+});
+
+window.addEventListener('turbo:load', () => {
+  console.log("OK");
 });
