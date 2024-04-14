b5452ac1f0d5c69a4b429d38204d3d16108e1411
wonderfullworld5 <bbb893576@gmail.com>
2024-04-14 14:34:45 +0900

    削除　挙動確認

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,25 +2,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
 import "item_price"
-// import "@rails/ujs"
-// Rails.start()
-// document.addEventListener('wheel', function(event) {
-//   // ここでスクロールに関連する処理を行う
-// }, { passive: true })
-// window.addEventListener('wheel', event => {
-//   // ここでスクロールに関連する処理を行う
-// }, { passive: true });
-// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//   // 非同期処理を行う関数
-//   someAsynchronousOperation(request, (error, result) => {
-//       if (error) {
-//           console.error('Error during async operation', error);
-//           sendResponse({ success: false, error: error.toString() });
-//       } else {
-//           sendResponse({ success: true, data: result });
-//       }
-//   });
-
-//   // 非同期レスポンスを返すためにtrueを返す
-//   return true;
-// });
