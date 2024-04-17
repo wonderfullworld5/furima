1c5274da08484a8f9c5ab6a9fd630ab2ca97f9e5
wonderfullworld5 <bbb893576@gmail.com>
2024-04-18 05:11:04 +0900

    エラー修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -5,16 +5,16 @@ class RecordsController < ApplicationController
   end
 
   def new_order
-    @purchase_form = PurchaseForm.new(item_id: @item.id)
-    render 'records/index' # app/views/records/index.html.erb
+    @purchase_form = PurchaseForm.new
+    render 'index'  # index.html.erb
   end
 
   def create_order
     @purchase_form = PurchaseForm.new(purchase_form_params)
     if @purchase_form.save
-      redirect_to success_item_record_path(@item)
+      redirect_to success_item_records_item_records_path(@item), notice: '購入が完了しました。'
     else
-      render :new_order
+      render :index
     end
   end
 
@@ -28,3 +28,4 @@ class RecordsController < ApplicationController
     params.require(:purchase_form).permit(:postcode, :area_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,13 @@ Rails.application.routes.draw do
 
   resources :items do
     resources :records, only: [:index, :create] do
-      get 'new_order', on: :member
-      post 'create_order', on: :member
-      get 'success', on: :member, as: 'success' 
+      member do
+        get 'new_order'  # new_order をメンバールートとして設定
+        post 'create_order'
+      end
+      collection do
+        get 'success', as: 'success_item_records'
+      end
+    end
   end
-end
 end
