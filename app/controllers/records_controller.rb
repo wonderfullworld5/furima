c261d0401fa12e02b19a8aaa6e7042a55189ac39
wonderfullworld5 <bbb893576@gmail.com>
2024-04-19 07:24:14 +0900

    エラー箇所の修正

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,20 +1,18 @@
 class RecordsController < ApplicationController
-  before_action :set_item, only: [:new_order, :create_order]
+  before_action :set_item, only: [:index, :create]
 
   def index
     @purchase_form = PurchaseForm.new
   end
 
   def create
-    @purchase_form = PurchaseForm.new(purchase_params)
-    if @purchase_form.valid?
-      # 購入処理
+    @purchase_form = PurchaseForm.new(purchase_form_params)
+    if @purchase_form.save
       redirect_to root_path
     else
       render :index, status: :unprocessable_entity
     end
   end
-
 
   private
 
@@ -23,7 +21,7 @@ class RecordsController < ApplicationController
   end
 
   def purchase_form_params
-    params.require(:purchase_form).permit(:postcode, :area_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: @item.id)
+    params.require(:purchase_form).permit(:postcode, :area_id, :city, :street, :building, :phone).merge(user_id: current_user.id, item_id: params[:item_id])
+    end
   end
-end
 
