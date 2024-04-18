ffe29a1367afdfaee2dde07c9f27235dbd137ba2
wonderfullworld5 <bbb893576@gmail.com>
2024-04-19 08:28:21 +0900

    バリデーション追加

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,5 +2,10 @@ class Record < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :ship
+
+  # バリデーション
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
 end
 
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,3 +1,11 @@
 class Ship < ApplicationRecord
   belongs_to :record
+
+  # バリデーション
+  validates :postcode, presence: true, format: { with: /\A\d{3}-\d{4}\z/}
+  validates :area_id, presence: true
+  validates :city, presence: true
+  validates :street, presence: true
+  validates :phone, presence: true, format: { with: /\A\d{10,11}\z/}
 end
+
